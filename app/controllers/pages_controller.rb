f318351472d4caa9d68cda9c6a98a11a0b76f543
PedroBritoWR <pedrowpvh@hotmail.com>
2024-06-12 14:00:44 -0400

    estilizando paginas de cadastro de novos produtos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def sobreNos
   end
+
+  def produtosCadastrados
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
   get '/sobreNos', to: 'pages#sobreNos'
+  get '/produtosCadastrados', to: 'pages#produtosCadastrados'
   root 'home#index'
 end
