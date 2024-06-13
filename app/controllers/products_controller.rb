f37e5c651defae7840a7799fa4c54b4b45e4f262
PedroBritoWR <pedrowpvh@hotmail.com>
2024-06-13 09:53:18 -0400

    listagem de tabela dos produtos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,7 @@ class ProductsController < ApplicationController
 
   # GET /products/1 or /products/1.json
   def show
+    @products = Product.all # Adicione esta linha para definir @products
   end
 
   # GET /products/new
